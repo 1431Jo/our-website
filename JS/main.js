c6e5895c6fd48b2ec0791455e8d3e2e03dd5c189
Nic2030 <jangraefenstein@icloud.com>
2020-11-13 22:03:35 +0100

    Update style.css

--- JS/main.js
+++ JS/main.js
@@ -1,2 +1,9 @@
 //main für Index.html
 
+function TextFlaps(flaps) {
+    if(document.getElementById(flaps).style.display === "none") {
+        document.getElementById(flaps).style.display = "inline";
+    } else {
+        document.getElementById(flaps).style.display = "none";
+    }
+ }
